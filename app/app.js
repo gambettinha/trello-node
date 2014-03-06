84d954f3cb8784e1782b811ef49073ca72bbec46
Rafael Salerno <salerno.rafael@gmail.com>
2014-03-06 16:15:23 -0300

    paser test info

--- app/app.js
+++ app/app.js
@@ -9,6 +9,8 @@ app.use(express.bodyParser());
 app.get('/hello.txt', function(req, res) {
   res.send('Hello World');
 });
+
+app.get('/new_call', service.new_call);
 app.get('/raw_api_call', service.rawApiCall);
 app.get('/simpler_api_call', service.simplerApiCall);
 app.get('/project/:pIndex', service.projectByIndex);
