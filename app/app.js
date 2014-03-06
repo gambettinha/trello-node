e428ac65cf77b25e4c3eb5c9a9a672bbde2a616e
Joel Correa <joel.correa@ilegra.com>
2014-03-06 16:10:32 -0300

    Added sample angular project

--- app/app.js
+++ app/app.js
@@ -3,12 +3,58 @@
 var express = require('express');
 var app = express();
 var service = require('./service');
+var routes = require('./routes');
+var api = require('./routes/api');
+
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.set('view options', {
+    layout: false
+  });
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
+});
+
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
 
 app.use(express.bodyParser());
+
+
+
+// Routes
+
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// JSON API
+
+app.get('/api/posts', api.posts);
+
+app.get('/api/post/:id', api.post);
+app.post('/api/post', api.addPost);
+app.put('/api/post/:id', api.editPost);
+app.delete('/api/post/:id', api.deletePost);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
 
 app.get('/hello.txt', function(req, res) {
   res.send('Hello World');
 });
+
 app.get('/raw_api_call', service.rawApiCall);
 app.get('/simpler_api_call', service.simplerApiCall);
 app.get('/project/:pIndex', service.projectByIndex);
